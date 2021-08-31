7b54996301a46f9459131a980ab55e3811e4f171
scawp <rob@scawp.com>
2021-08-31 12:14:26 +0200

    added confirm to run script

--- run.sh
+++ run.sh
@@ -16,7 +16,14 @@ source_dir="/mnt/c/Users/rob/Pictures/crap from z3/WhatsApp Images"
 destintion_dir="/mnt/p/photos/"
 
 #run the scripts
-
 "$scripts_dir/1-find-and-log-matches.sh" "$source_dir" "$run_id"
 "$scripts_dir/2a-generate-new-filepathnames-from-name.sh" "$tmp_dir/1-find-and-log-matches-$run_id.log" "$destintion_dir" "$run_id"
-#"$scripts_dir/3-create-new-files-and-paths.sh" "$tmp_dir/tmp/2-generate-new-filepathnames-$run_id.log"
+
+echo -n "Do you want to process the files contained in "$tmp_dir/1-find-and-log-matches-$run_id.log" ? (y/n) "
+read confirm
+if [ "$confirm" = "y" ]; then
+  echo "running"
+  "$scripts_dir/3-create-new-files-and-paths.sh" "$tmp_dir/2-generate-new-filepathnames-$run_id.log"
+else
+  echo -e "Quitting, to continue, run:\n"$scripts_dir/3-create-new-files-and-paths.sh" "$tmp_dir/2-generate-new-filepathnames-$run_id.log""
+fi
